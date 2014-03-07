fdf13740b34651d1bb2a78d2de506fdb002ed2ff
Diego <rushpe@gmail.com>
2014-03-07 18:40:01 -0500

    main.m

---
+++ HelloShe/HelloShe/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloShe
+//
+//  Created by Diego M. Urquiza on 3/7/14.
+//  Copyright (c) 2014 Society of Hispanic Engineers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
