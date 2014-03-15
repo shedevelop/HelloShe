9d7082fa716ee718dcd91df24311011b974cd35f
Evan Arbeitman <evanarbeitman@gmail.com>
2014-03-14 20:26:54 -0400

    Evan main.n

---
+++ main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Evan Arbeitman on 3/7/14.
+//  Copyright (c) 2014 Evan Arbeitman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
+// Test
+// Test2
+// Test3
+// Hello
